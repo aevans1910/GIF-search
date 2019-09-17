df6490c15df4fd066f26a05ef0dcfee7d7e29a1e
Ariane Evans <ariane.evans@student.makeschool.com>
2019-09-16 22:28:15 -0700

    [add] BEW code to display gifs

--- app.py
+++ app.py
@@ -7,22 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
+    # Test
+    print("entered route")
     # TODO: Extract the query term from url using request.args.get()
+    term = request.args.get('term')
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
 
+    params = {"q": term, "key": "BG41AB43M6OC", 'limit': 1}
+    gifs = None
+
     # TODO: Make an API call to Tenor using the 'requests' library. For 
     # reference on how to use Tenor, see: 
     # https://tenor.com/gifapi/documentation
+    r = requests.get("https://api.tenor.com/v1/search", params)
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
+    if r.status_code == 200:
+        gifs = r.json()
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
+    results = gifs["results"]
+    print (results)
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
